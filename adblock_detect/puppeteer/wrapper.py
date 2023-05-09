776cae92d24d38be4f642b55cd20452384993153
Racro <ritik.r@nyu.edu>
2023-05-09 18:24:59 -0400

    adblock_detect on custom list

--- adblock_detect/puppeteer/wrapper.py
+++ adblock_detect/puppeteer/wrapper.py
@@ -91,7 +91,7 @@ def run(sites, extn, return_dict, l):
 
 if __name__ == "__main__":
     try:
-        with open("../inner_pages.json", "r") as f:
+        with open("../inner_pages_custom.json", "r") as f:
             updated_dict = json.load(f)
         f.close()
         # with open("../failed_sites.txt", "r") as f:
@@ -145,13 +145,13 @@ if __name__ == "__main__":
         # json.dump(result_dict, f)
         # f.close()
 
-        f = open("adblock_detect.json", "w")
+        f = open("adblock_detect_custom.json", "w")
         json.dump(result_dict, f)
         f.close()
     except KeyboardInterrupt:
         print('Interrupted')
         
-        f = open("adblock_detect.json", "w")
+        f = open("adblock_detect_custom.json", "w")
         json.dump(result_dict, f)
         f.close()
         
