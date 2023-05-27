9528d9d209a1923b6e6061d375bbc907ced62a6c
Racro <ritik.r@nyu.edu>
2023-05-27 11:09:48 -0400

    restructuring folders

--- performance/cpu_load/process_cpu_data.py
+++ performance/cpu_load/process_cpu_data.py
@@ -8,53 +8,70 @@ import matplotlib.pyplot as plt
 import sys
 
 # list of all files in /data folder
-# path = f"./docker/{args.browser}/data"
-# path = f"./docker/chrome/data/"
-path = f"./data_1000/data/"
+path = f"./data_1000/data_custom/"
 dir_list = os.listdir(path)
 # dir_list = dir_list[:6000]
 
-extn_lst = ['control', 'adblock', 'ublock', 'privacy-badger']
-# extn_lst = ['control', 'adblock', 'ublock', 'privacy-badger',
-#        "decentraleyes",
-#        "disconnect",
-#        "ghostery",
-#        "https",
-#        "noscript",
-#        "scriptsafe",
-#        "canvas-antifp",
-#        "adguard"]
+# extn_lst = ['control', 'adblock', 'ublock', 'privacy-badger']
+extn_lst = ['control', 'adblock', 'ublock', 'privacy-badger',
+       "decentraleyes",
+       "disconnect",
+       "ghostery",
+       "https",
+       "noscript",
+       "scriptsafe",
+       "canvas-antifp",
+       "adguard"]
 
 data_dict = {
     'websites': []
 }
 
 for extn in extn_lst:
-    data_dict[extn] = []
-
-# data_dict = {'websites': [list_of_websites], 'extn_lst[i]': [list of [usr, sys, iowait, stats]]}
+    data_dict[extn] = [] # data_dict = {'websites': [list_of_websites], 'extn_lst[i]': [list of [usr, sys, iowait, stats]]}
 
 faulty_sites = 0
 faulty_extn = {}
 for extn in extn_lst[1:]:
     faulty_extn[extn] = 0
-# 'adblock': 0, 'ublock': 0, 'privacy-badger': 0
 
-def check_for_keys(key_lst, lst):
+def check_for_keys(key_lst, lst): # key_lst -> list of keys in a website json object, lst -> (extn_lst + data/website)
     a = 0
     global faulty_sites
+    faulty_lst = []
     for i in lst:
         if i not in key_lst:
             a = 1
             if i[:5] == "/data":
                 faulty_sites += 1
+                faulty_lst = []
+                break
             else:
                 faulty_extn[i] += 1
-            # print(lst[-1], i)
+                faulty_lst.append(i)
     if a:
-        return False
+        return False, faulty_lst
     else:
-        return True
+        return True, faulty_lst
+
+def sort(feature_dict):
+    zipped = zip(feature_dict[extn_lst[0]], feature_dict[extn_lst[1]], feature_dict[extn_lst[2]], feature_dict[extn_lst[3]])    
+    sorted_zipped = sorted(zipped)
+    unzipped = list(zip(*sorted_zipped))
+    for i in range(len(extn_lst)):
+        feature_dict[extn_lst[i]] = list(unzipped[i])
+    return feature_dict
+
+class NpEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.integer):
+            return int(obj)
+        if isinstance(obj, np.floating):
+            return float(obj)
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        return super(NpEncoder, self).default(obj)
+
 
 for website in dir_list:
     f = open(path+website, 'r')
@@ -67,12 +84,16 @@ for website in dir_list:
     # checking for dict key values
     extns = extn_lst[1:] 
     extns.append(key)
-    check_val = check_for_keys(data['stats'].keys(), extns)
+    check_val, faulty_lst = check_for_keys(data['stats'].keys(), extns)
 
     if (check_val == False):
         print("----------------- check this -------------------")
-        print(website)
-        continue #revisit this. should only continue in ctrl case not found
+        if faulty_lst != []:
+            for extn in faulty_lst:
+                data["stats"][extn] = {"webStats": [-1, -1]}
+        else:
+            print(website)
+            continue # only continue in ctrl case not found
     data_dict['websites'].append(website)
 
     try:
@@ -107,18 +128,13 @@ for website in dir_list:
             pass
 
 # print(data_dict)
-# data_dict = {'websites':[], 'k1': [[[v1,v2,v3], [v4,v5,v6], [v1,v2,v3]], [[v1,v2,v3], [v4,v5,v6], [v1,v2,v3]]], 'k2': [[[v1,v2,v3], [v4,v5,v6], [v1,v2,v3]], [[v1,v2,v3], [v4,v5,v6], [v1,v2,v3]]], 'k3': [[[v1,v2,v3], [v4,v5,v6], [v1,v2,v3]], [[v1,v2,v3], [v4,v5,v6], [v1,v2,v3]]]}
-
-# import sys
-# print(faulty_sites)
-# print(faulty_extn)
-# sys.exit(0)
+# data_dict = {'websites':[], 'k1': [[[v1,v2,v3,ws1], [v4,v5,v6,ws2], [v1,v2,v3,ws3]], [[v1,v2,v3,ws11], [v4,v5,v6,ws21], [v1,v2,v3, ws31]]], 'k2': [[[v1,v2,v3], [v4,v5,v6], [v1,v2,v3]], [[v1,v2,v3], [v4,v5,v6], [v1,v2,v3]]], 'k3': [[[v1,v2,v3], [v4,v5,v6], [v1,v2,v3]], [[v1,v2,v3], [v4,v5,v6], [v1,v2,v3]]]}
 
 max_plot = [{}, {}, {}] # for usr, sys, iowait
 avg_plot = [{}, {}, {}]
 stat_plot = [{}, {}]
 
-for i in range(4):
+for i in range(4): # initialization
     for k in data_dict:
         if k != 'websites':
             if (i == 3):
@@ -146,111 +162,71 @@ for i in range(len(data_dict['control'])):
                     #avg
                     avg_plot[j][k].append(sum(data_dict[k][i][j]) / len(data_dict[k][i][j])) # can do [:-1] so that last entry can be ignored (which would mostly be close to 0) bcoz I did run mpstat for 2 extra cycle 
 
-
-def sort(feature_dict):
-    zipped = zip(feature_dict[extn_lst[0]], feature_dict[extn_lst[1]], feature_dict[extn_lst[2]], feature_dict[extn_lst[3]])    
-    sorted_zipped = sorted(zipped)
-    unzipped = list(zip(*sorted_zipped))
-    for i in range(len(extn_lst)):
-        feature_dict[extn_lst[i]] = list(unzipped[i])
-    return feature_dict
-
-class NpEncoder(json.JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, np.integer):
-            return int(obj)
-        if isinstance(obj, np.floating):
-            return float(obj)
-        if isinstance(obj, np.ndarray):
-            return obj.tolist()
-        return super(NpEncoder, self).default(obj)
-
 def generate_stats_dict(data_dict):
     websites = np.array(data_dict['websites'])
-    ctrl_stat = np.array(stat_plot[1]['control'])
-    abp_stat = np.array(stat_plot[1]['adblock'])
-    ub_stat = np.array(stat_plot[1]['ublock'])
-    pb_stat = np.array(stat_plot[1]['privacy-badger'])
-
+    # extn_stat = []
     dele = {}
-    dele['ctrl'] = ctrl_stat
-    dele['abp'] = abp_stat
-    dele['ub'] = ub_stat
-    dele['pb'] = pb_stat
-
     d = {}
-    d['ctrl'] = {}
-    d['abp'] = {}
-    d['ub'] = {}
-    d['pb'] = {}
-
-    # filter all -1 values from stat_plot
-    for i in d.keys():
-        if i == 'ctrl':
+    for extn in extn_lst:
+        if extn == 'control':
             continue
-        dummy = ctrl_stat
-        index = np.where(dele[i] == 2437)
-        print(index)
-        print(dele[i][index])
-        print(dummy[index])
-        print(websites[index])
-        np.delete(dele[i], index)
+
+        # extn_stat.append(np.array(stat_plot[1][extn]))
+        dele[extn] = np.array(stat_plot[1][extn])
+        d[extn] = {}
+
+        # filter all -1 values from stat_plot
+        dummy = np.array(stat_plot[1]['control'])
+        index = np.where(dele[extn] == -1)
+        # print(index)
+        # print(dele[extn][index])
+        # print(dummy[index])
+        # print(websites[index])
+        np.delete(dele[extn], index)
         np.delete(dummy, index)
-        sys.exit(0)
-        zipped = zip(dele[i] - dummy, websites)
+        zipped = zip(dele[extn] - dummy, websites)
         sorted_zipped = sorted(zipped)
         unzipped = list(zip(*sorted_zipped))
         x = list(unzipped[1])
         y = list(unzipped[0])
         for j in range(len(x)):
-            d[i][x[j]] = y[j]
+            d[extn][x[j]] = y[j]
+    
     import json
-    with open('website_categorization/site_load_time_1000.json', 'w') as f:
+    with open('website_categorization/site_load_time_custom_1000.json', 'w') as f:
         json.dump(d, f, cls=NpEncoder)
     f.close()
 generate_stats_dict(data_dict)
 
-# print(np.sort(abp_stat-ctrl_stat))
-# plt.plot(np.sort(ub_stat-ctrl_stat), label = 'abp')
+
 # plt.axhline(np.mean(ub_stat-ctrl_stat), linestyle='dashed', color='g')
-# plt.legend()
-# plt.show()
 sys.exit(0)
 
+avg_np = {}
+max_np = {}
+for extn in extn_lst:
+    avg_np[extn] = np.array(avg_plot[0][extn])
+    max_np[extn] = np.array(max_plot[0][extn])
 
-# max_sort
-# print(max_plot[0]['control'])
-# print(sort(max_plot[0])['control'])
+def plot_max():
+    for extn in max_np.keys():
+        if extn != 'control':
+            plt.plot(np.sort(max_np[extn] - max_np['control']), label = extn)
+            plt.legend()
+            plt.savefig(f'max_{extn}.png')
 
-# for k in max_plot[0]:
-# plt.plot(max_plot[0]['control'], label = 'control')
-# plt.plot(max_plot[0]['adblock'], label = 'adblock')
-# plt.plot(max_plot[0]['ublock'], label = 'ublock')
-# plt.plot(max_plot[0]['privacy-badger'], label = 'privacy-badger')
+def plot_avg():
+    for extn in avg_np.keys():
+        if extn != 'control':
+            plt.plot(np.sort(avg_np[extn] - avg_np['control']), label = extn)
+            plt.legend()
+            plt.savefig(f'avg_{extn}.png')
 
-ctrl_np = np.array(avg_plot[0]['control'])
-abp_np = np.array(avg_plot[0]['adblock'])
-ub_np = np.array(avg_plot[0]['ublock'])
-pb_np = np.array(avg_plot[0]['privacy-badger'])
-
-abp = [len(np.where((abp_np-ctrl_np) > 0)[0]), len(np.where((abp_np-ctrl_np) < 0)[0])]
-ub = [len(np.where((ub_np-ctrl_np) > 0)[0]), len(np.where((ub_np-ctrl_np) < 0)[0])]
-pb = [len(np.where((pb_np-ctrl_np) > 0)[0]), len(np.where((pb_np-ctrl_np) < 0)[0])]
-
-print(len(ctrl_np))
-print('abp[>0, <0] - ', abp)
-print('ub[>0, <0] - ', ub)
-print('pb[>0, <0] - ', pb)
-
-
-
-# print(len(ctrl_np))
 # print("abp:", np.average(np.sort(abp_np-ctrl_np)))
-# print("ub:", np.average(np.sort(ub_np-ctrl_np)))
-# print("pb:", np.average(np.sort(pb_np-ctrl_np)))
+# abp = [len(np.where((abp_np-ctrl_np) > 0)[0]), len(np.where((abp_np-ctrl_np) < 0)[0])]
 
 
-plt.plot(np.sort(abp_np), label = 'abp')
-plt.legend()
-plt.show()
+
+
+# plt.show()
 # plt.save('lineplot.png')
